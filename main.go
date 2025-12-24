0aa8a4bc4398e16de296e6080522cc4a76f088de
Guilhermee-ds <contato.guilhermedossantos@gmail.com>
2025-12-24 12:01:31 -0300

    feat: implement update tasks for api

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -74,12 +75,46 @@ func listTasks(c *gin.Context) {
 	c.JSON(http.StatusOK, tasks)
 }
 
+func updateTask(c *gin.Context) {
+	idParam := c.Param("id")
+	objID, err := primitive.ObjectIDFromHex(idParam)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	var task models.Task
+
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	result, err := collection.UpdateOne(ctx, bson.M{"_id": objID}, bson.M{"$set": task})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	if result.MatchedCount == 0 {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Task not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Task updated"})
+}
+
 func main() {
 	connectDB()
 	router := gin.Default()
 
 	router.POST("/task", createTask)
 	router.GET("/tasks", listTasks)
+	router.PUT("/task/:id", updateTask)
 
 	router.Run(":8080")
 }
